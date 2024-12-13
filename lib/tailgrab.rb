b42d66c517d64fabb22562b8ecd3deeea73af234
Himanshu Panwar <hpanwar@g2.com>
2024-12-13 23:14:30 +0530

    first commit

---
+++ lib/tailgrab.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative "tailgrab/version"
+
+class Tailgrab
+  SHEET = "#{ENV['HOME']}/.tailgrab.txt".freeze
+
+  def self.parse(args)
+
+  end
+
+  def self.save_to_file
+
+  end
+
+  def self.read_from_file
+
+  end
+
+  def self.clear_file
+
+  end
+
+  def self.print_file
+
+  end
+
+  def self.print_help
+
+  end
+
+  def self.blank_file?
+
+  end
+
+end
---
+++ lib/tailgrab/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tailgrab
+  VERSION = "0.1.0"
+end
